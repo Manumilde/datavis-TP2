04a0a01d64edab2f29924771bceba11ee6bef367
Manumilde <101014747+Manumilde@users.noreply.github.com>
2024-05-12 02:14:24 -0300

    Add files via upload

---
+++ src/components/AxisX.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let height;
+    export let width;
+    export let xScale;
+    export let margin;
+    export let yScale;
+
+    let xTicks = [1, 5, 10, 20];
+    let orbit = [1, 3, 7, 12];
+</script>
+
+<g>
+    {#each xTicks as tick}
+        <text 
+        x= {width -width +  margin.left}
+        y={yScale(height)}
+        dx="2"
+        dy="10"
+        style="font-size: small; "
+        >{tick}km</text>
+        
+    
+    {/each}
+
+</g>
---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,51 @@
+<script>
+    export let data;
+    export let transvertx;
+    export let transverty;
+</script>
+
+<div class = "tooltip" style="position: absolute;
+    top: {(transverty(data.ejeY,data.distancia)*55)+500}px;
+    left: {(transvertx((data.ejeY),data.distancia)*55)+13+400}px;">
+
+    <h3>{data.nombre}</h3>
+    <p>{data.Tiempo}</p>
+</div>
+
+<!-- <div class = "tooltip" style="position: absolute;
+    top: {450+650}px;
+    left: {(600)+650}px;">
+
+    <h3>{data.nombre}</h3>
+    <p>{data.Tiempo}</p>
+</div> -->
+
+
+<!-- Para coso fijo -->
+
+<!-- <div class = "tooltip" style="position: fixed;
+    bottom: 0;
+    right: 0;">
+
+    <h3>{data.nombre}</h3>
+    <p>{data.Tiempo}</p>
+</div> -->
+
+<style>
+    .tooltip {
+        padding: 6px;
+        
+        background-color: rgba(0, 0, 0, .5);
+        
+        pointer-events: none;
+        transition: top 300ms ease, left 300ms ease;
+        border-radius: 10px;
+        color: wheat;
+
+
+    }
+    .h3 {
+        font-weight: 700;
+        margin-bottom: 0.25rem;
+    }
+</style>
